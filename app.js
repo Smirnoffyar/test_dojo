6b9ec7c08b326ff30ccbb32449be12d639d6cfb7
Smirnoffyar <smirnoffyar76@mail.ru>
2022-04-07 23:04:29 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+let openPopupHeader = function (state) {
+    document.querySelector("#filter_header").style.display = state;
+    document.querySelector("#header").style.display = state;
+};
+
+let openPopupFooter = function (state) {
+    document.querySelector("#filter_footer").style.display = state;
+    document.querySelector("#footer").style.display = state;
+};
